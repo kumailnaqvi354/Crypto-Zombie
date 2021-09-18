a093ecd14ce88f9ae9beb165745d70e63ab2f7f2
kumailnaqvi354 <komailnaqvi616@gmail.com>
2021-09-18 22:22:51 +0500

    truffle config updated for deployment on different network

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,14 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const LoomTruffleProvider = require('loom-truffle-provider');
+
+const infuraKey = "b713d93f58014bf5bfd4ec9a139b0149";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const PRIVATE_KEY = fs.readFileSync(".privatekey").toString().trim();
 
 module.exports = {
   /**
@@ -47,16 +50,17 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-    // loom_testnet: {
-    //   provider: function() {
-    //     const privateKey = 'YOUR_PRIVATE_KEY';
-    //     const chainId = 'extdev-plasma-us1';
-    //     const writeUrl = 'wss://extdev-basechain-us1.dappchains.com/websocket';
-    //     const readUrl = 'wss://extdev-basechain-us1.dappchains.com/queryws';
-    //     return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
-    //   },
-    //   network_id: 'extdev'
-    // }
+    
+    loom_testnet: {
+      provider: function() {
+        const privateKey = PRIVATE_KEY
+        const chainId = 'extdev-plasma-us1';
+        const writeUrl = 'http://extdev-plasma-us1.dappchains.com:80/rpc';
+        const readUrl = 'http://extdev-plasma-us1.dappchains.com:80/query';
+        return new LoomTruffleProvider(chainId, writeUrl, readUrl, privateKey);
+        },
+      network_id: '9545242630824'
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -68,14 +72,14 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    ropsten: {
+    provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/infuraKey`),
+    network_id: 3,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
